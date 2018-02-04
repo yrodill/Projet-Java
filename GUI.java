23cc1fda3041e01ce3f1e54546731feea27886f4
Pierre Jacquet <peepanjacquet@gmail.com>
2018-02-04 17:35:48 +0100

    update

--- GUI.java
+++ GUI.java
@@ -1,19 +1,21 @@
-import java.awt.EventQueue;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Color;
-
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
 import java.util.*;
 // import java.util.ArrayList;
 // import java.util.Arrays;
-// import java.util.List;
+import java.util.List;
 
 //import javax.lang.model.util.SimpleTypeVisitor6;
+import javax.swing.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JColorChooser;
+
+import javax.imageio.ImageIO;
 
 /**
 * https://stackoverflow.com/questions/7702697/how-to-get-x-and-y-index-of-element-inside-gridlayout
@@ -25,6 +27,11 @@ public class GUI {
     private final List<JButton> list = new ArrayList<JButton>();
     public int click = 0;
     public Case clicked;
+    public ImageIcon mito = new ImageIcon("./mito_sized.png");
+    public ImageIcon lipid = new ImageIcon("./lipid.png");
+    public ImageIcon enzyme = new ImageIcon("./enzyme.png");
+    
+    
 
     private JButton getGridButton(int r, int c) {
         int index = r * N + c;
@@ -33,18 +40,20 @@ public class GUI {
 
     private JButton createGridButton(final int row, final int col) {
         final JButton b;
-
         int index = (row * 15 + col);
         Case pieceaposer = Plateau.plateau.get(index);
         String type = pieceaposer.get_type();
         Boolean highlighted = pieceaposer.is_highlighted();
         Boolean reachable = pieceaposer.is_reachable();
 
-        b = new JButton(type);
+        b = new JButton();
+
         if (index % 2 == 0) {
-            b.setBackground(Color.WHITE);
-            b.setForeground(Color.BLACK);
-            b.setOpaque(true);
+            // b.setBackground(Color.WHITE);
+            // b.setForeground(Color.BLACK);
+            b.setOpaque(false);
+            b.setContentAreaFilled(false);
+            b.setHorizontalAlignment(SwingConstants.CENTER);
         }
         if (index % 2 == 1) {
             b.setBackground(Color.BLACK);
@@ -52,8 +61,13 @@ public class GUI {
             b.setOpaque(true);
         }
         if (type.equals("M")) {
-            b.setForeground(Color.BLUE);
-            b.setOpaque(true);
+            b.setIcon(mito);
+        }
+        if (type.equals("L")) {
+            b.setIcon(lipid);
+        }
+        if (type.equals("E")) {
+            b.setIcon(enzyme);
         }
         if (highlighted) {
             b.setBackground(Color.GREEN);
@@ -81,13 +95,12 @@ public class GUI {
                 if (click == 2) {
                     Case target = Plateau.plateau.get(index);
                     System.out.println("Atteignable: " + target.is_reachable());
-                    
-                    if (target.is_thesameposition(clicked)){
+
+                    if (target.is_thesameposition(clicked)) {
                         clicked.highlight(false);
                         unreachable();
                         click = 0;
-                    }
-                    else if (target.is_reachable()) {
+                    } else if (target.is_reachable()) {
                         int old_row = clicked.get_row();
                         int old_col = clicked.get_col();
                         int old_index = (old_row * 15 + old_col);
@@ -105,7 +118,7 @@ public class GUI {
                         clicked.highlight(false);
                         unreachable();
                         click = 0;
-                    }else {
+                    } else {
                         System.out.println("Case inaccessible!");
                         click = 1;
                     }
@@ -141,10 +154,11 @@ public class GUI {
         }
     }
 
-  
-
     public JPanel createGridPanel() {
         JPanel p = new JPanel(new GridLayout(N, N));
+        p.setPreferredSize(new Dimension(810, 810));
+        p.setVisible(true);
+        p.setOpaque(false);
         for (int i = 0; i < N * N; i++) {
             int row = i / N;
             int col = i % N;
@@ -159,20 +173,24 @@ public class GUI {
         f.getContentPane().removeAll();
         list.clear();
         display();
-        // for (int i = 0; i < Plateau.plateau.size(); i++) {
-        //     Plateau.plateau.get(i).affiche();
-        // }
     }
 
-    JFrame f = new JFrame("GridButton");
+    JFrame f = new JFrame("JPlusPlus");
 
     public boolean init = true;
 
     public void display() {
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //f.setResizable(false);
         f.add(createGridPanel());
         f.pack();
-        f.setSize(800, 800);
+        //ImageIcon imageIcon = new ImageIcon("./background.jpg"); // load the image to a imageIcon
+        //Image image = imageIcon.getImage(); // transform it 
+        //Image newimg = image.getScaledInstance(800, 800,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+        //imageIcon = new ImageIcon(newimg);
+        //JLabel background = new JLabel(imageIcon);
+        //f.add(background);
+
         if (init) {
             f.setLocationRelativeTo(null);
         }
