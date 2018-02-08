ac974ff3e408d5cb758517388c2bfd1031916b3a
yrodill <benbotho@gmail.com>
2018-02-08 11:53:28 +0100

    deplacmeent in Plateau and pseudo fixed metabo

--- Plateau.java
+++ Plateau.java
@@ -128,13 +128,109 @@ public class Plateau {
         }
     }
 
-    public static void move_metabolite() {
 
-        Iterator<Case> cases_selected = plateau.iterator();
-        while (cases_selected.hasNext()) {
+        public static boolean test_move(int move){
+          if(move >= 0 && move <225){
+            return true;
+          }
+          return false;
+        }
+
+    public static void move_lipides(Case clicked){
+
+
+          Case selected = clicked;
+
+            if (selected instanceof Lipid) {
+                int row = selected.get_row();
+                int col = selected.get_col();
+                int possible_case = 0;
+                //System.out.println(row + "," + col);
+                for (int i = 1; i <= 3; i++) {
+                    if (selected.get_Player() == 1) {
+                        possible_case = row + i;
+                    } else if (selected.get_Player() == 2) {
+                        possible_case = row - i;
+                    }
+                    int index = possible_case * 15 + col;
+                    String type = Plateau.plateau.get(index).get_type();
+                    if (type.equals(" ")) {
+                        Plateau.plateau.get(index).set_reachable(true);
+                    } else {
+                        Plateau.plateau.get(index).set_reachable(false);
+                        break;
+                    }
+                }
+              }
+            }
+
+        public static void move_enzyme(Case clicked){
+
+              Case selected = clicked;
+
+            if(selected instanceof Enzyme){
+              List<Integer> move_possible_enzyme = new ArrayList<Integer>();
+              int row = selected.get_row();
+              int col = selected.get_col();
+              int move,move2,move4,move5,move7,move8;
+              move=0;move2=0;move4=0;move5=0;move7=0;move8=0;
+              if(col != 0){
+                move = row*15-1+col; //gauche
+                move4 = (row-1)*15+col-1; //diagonale haut gauche
+                move7 = (row+1)*15+col-1; // diagonale bas gauche
+              }
+              if(col != 14){
+                move2 = row*15+1+col; //droite
+                move5 = (row-1)*15+col+1; // diagonale haut droite
+                move8 = (row+1)*15+col+1; //diagonale bas droite
+
+              }
+              int move3 = (row-1)*15+col; //haut
+              int move6 = (row+1)*15+col; // bas
+              if(test_move(move)){
+                move_possible_enzyme.add(move);
+              }
+              if(test_move(move2)){
+                move_possible_enzyme.add(move2);
+              }
+              if(test_move(move3)){
+                move_possible_enzyme.add(move3);
+              }
+              if(test_move(move4)){
+                move_possible_enzyme.add(move4);
+              }
+              if(test_move(move5)){
+                move_possible_enzyme.add(move5);
+              }
+              if(test_move(move6)){
+                move_possible_enzyme.add(move6);
+              }
+              if(test_move(move7)){
+                move_possible_enzyme.add(move7);
+              }
+              if(test_move(move8)){
+                move_possible_enzyme.add(move8);
+              }
+
+              for(int j=0;j<move_possible_enzyme.size();j++){
+                String type = Plateau.plateau.get(move_possible_enzyme.get(j)).get_type();
+                if(!type.equals("E")){
+                  Plateau.plateau.get(move_possible_enzyme.get(j)).set_reachable(true);
+                  }
+                  else{
+                  Plateau.plateau.get(move_possible_enzyme.get(j)).set_reachable(false);
+                  }
+              }
+            }
+          }
+
+
+
+    public static void move_metabolite(Case metaboSelected) {
+
             Vector<Vector> possible_move = new Vector();
-            Case selected = cases_selected.next();
-            //System.out.println(cases_selected.next());            
+            Case selected = metaboSelected;
+            //System.out.println(cases_selected.next());
             if (selected instanceof Metabolite) {
 
                 int row = selected.get_row();
@@ -154,7 +250,7 @@ public class Plateau {
                         possible_move.add(possible_coordinate);
                     }
                 }
-                
+
                 for (int i = 1; i < 4; i++) {
                     int possible_col = col - i; //gauche
                     int possible_index = row * 15 + possible_col;
@@ -213,7 +309,7 @@ public class Plateau {
             }
 
         }
-    }
+
 
     public static boolean bornee(int coord_xy) {
         if (coord_xy < 15 && coord_xy > -1) {
